d79d14ea98462766319b7ad53827d9a800fb29a2
Francois Flinois <francois.flinois@gmail.com>
2019-07-18 10:47:43 +0200

    maj

--- core/class/tter.class.php
+++ core/class/tter.class.php
@@ -224,7 +224,7 @@ class tter extends eqLogic {
 		log::add('tter','debug',$arrivee);
 		
 		// appel de l'API SNCF
-		log::add('tter','debug','trajets : '.SncfApi::getTrajets($apiKey,$depart,$arrivee)[0]['gareDepart']);
+		log::add('tter','debug','trajets : '.SncfApi::getTrajets($apiKey,$depart,$arrivee));
 		
 
 		// ##############################################################
